d6ac9e2b766ba80e25c9a54d1cd4a0d7fe7345c4
unknown <abarisic@protonmail.com>
2025-09-14 10:46:16 +0200

    typography setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-	return <>App</>;
+	return <div className="">App</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+
 import '@fontsource/dm-sans';
 import '@fontsource/bricolage-grotesque';
 import './index.css';
-import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
